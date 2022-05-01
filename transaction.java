e73bec50f7e31f12970c77845c9d45f10795f134
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2022-05-01 11:54:20 +0200

    Update transaction.java

--- transaction.java
+++ transaction.java
@@ -1,10 +1,9 @@
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
 import java.security.*;
-import java.security.spec.ECGenParameterSpec;
 import java.util.Base64;
 import java.security.spec.X509EncodedKeySpec;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.interfaces.RSAPublicKey;
+import java.security.interfaces.RSAPrivateKey;
 
 class transaction{
     String sender;
@@ -12,6 +11,7 @@ class transaction{
     double amount;
     String signature;
     
+    //Transaction Constructor
     public transaction(String sender, String reciever, double amount){
         this.sender = sender;
         this.reciever = reciever;
@@ -19,19 +19,18 @@ class transaction{
     }
     
     private String hashCalculate(){
+        
         String hash_source = this.sender + this.reciever + this.amount;
 
+        //Digesting the message
         MessageDigest md = null;
-        
         try {
             md = MessageDigest.getInstance("SHA-256");
-        } catch(Exception e){
-        }
-            
+        } catch(Exception e){}
 		md.update(hash_source.getBytes());
-		
 		byte[] hash_byte = md.digest();      
 
+        //Stringifing the hash
         StringBuffer hash_buffer = new StringBuffer();
       
         for (int i = 0; i < hash_byte.length; i++) {
@@ -44,43 +43,73 @@ class transaction{
     
     public void sign(String privateKeyStr){
         try{
-            byte[] privateBytes = Base64.getDecoder().decode(privateKeyStr);
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            PrivateKey privateKey = keyFactory.generatePrivate(new PKCS8EncodedKeySpec(privateBytes));
-            Signature privateSignature = Signature.getInstance("SHA256withRSA");
-            privateSignature.initSign(privateKey);
             
-            String trans_hash = hashCalculate();
+            //Getting PrivateKey from the String
+            byte[] privateBytes = Base64.getDecoder().decode(privateKeyStr);
+            KeyFactory keypr = KeyFactory.getInstance("RSA");
+            PrivateKey privateKey = keypr.generatePrivate(new PKCS8EncodedKeySpec(privateBytes));
+            
+            //Getting PublicKey from the String
+            byte[] publicBytes = Base64.getDecoder().decode(sender);
+            KeyFactory keypp = KeyFactory.getInstance("RSA");
+            PublicKey publicKey = keypp.generatePublic(new X509EncodedKeySpec(publicBytes));
+            
+            RSAPrivateKey rsaPri = (RSAPrivateKey) privateKey;
+            RSAPublicKey rsaPub = (RSAPublicKey) publicKey;
+
+            //Verifying PrivateKey with PublicKey
+            if(!rsaPri.getModulus().equals(rsaPub.getModulus()) ){
+                System.out.println("Incorrect Private Key\n");
+                signature = "0";
+                return;
+            }
+            
+            //Initializng signature
             Signature privateSign = Signature.getInstance("SHA256withRSA");
             privateSign.initSign(privateKey);
+            
+            //Calculating the hash for the transaction
+            String trans_hash = hashCalculate();
             privateSign.update(trans_hash.getBytes("UTF-8"));
-        
+            
+            //Signing the transaction with PrivateKey
             byte[] sign_bytes = privateSign.sign();
-        
             this.signature = Base64.getEncoder().encodeToString(sign_bytes);
             
         }catch(Exception e){}
     }
     
     public boolean verify(String signature, String publicKeyStr){
+        
+        //Verifying block isn't a genesis or reward block
         if (sender == null){
             return true;
         }
+        
+        //Verifying there is a signature for the transaction
         if (signature == null){
             return false;
         }
+        
         try{
+            
+            //Getting PublicKey from the String
             byte[] publicBytes = Base64.getDecoder().decode(publicKeyStr);
             KeyFactory keyFactory = KeyFactory.getInstance("RSA");
             PublicKey publicKey = keyFactory.generatePublic(new X509EncodedKeySpec(publicBytes));
             
-            Signature publicSignature = Signature.getInstance("SHA256withRSA");
-            publicSignature.initVerify(publicKey);
-            publicSignature.update(hashCalculate().getBytes("UTF-8"));
-        
+            //Initializng signature
+            Signature publicSign = Signature.getInstance("SHA256withRSA");
+            publicSign.initVerify(publicKey);
+            
+            //Calculating the hash for the transaction
+            String trans_hash = hashCalculate();
+            publicSign.update(trans_hash.getBytes("UTF-8"));
+            
+            //Verifying publicKey with the signature
             byte[] sign_bytes = Base64.getDecoder().decode(signature);
-        
-            return publicSignature.verify(sign_bytes);
+            return publicSign.verify(sign_bytes);
+            
         }catch(Exception e){}
         
         return false;
